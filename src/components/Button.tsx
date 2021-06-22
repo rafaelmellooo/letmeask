40149a1c027c9ff5dbf607f7cc21a281f34b6fbb
Rafael Mello <rafaelmello0715@outlook.com>
2021-06-22 04:23:42 -0300

    feat: add firebase

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
+
 export function Button() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <button type="button">Clique aqui o/</button>
+    <button type="button" onClick={() => setCounter(counter + 1)}>{counter}</button>
   );
 }

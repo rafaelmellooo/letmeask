40e4e51f11e84b22412c00a9eafe2f4cf9a6f7a0
Rafael Mello <rafaelmello0715@outlook.com>
2021-06-28 02:09:57 -0300

    feat: adição do react-router-dom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
@@ -26,7 +28,7 @@ export function NewRoom() {
           <p>
             Quer entrar em uma sala existente?
             {' '}
-            <a href="https://www.google.com/">Clique aqui</a>
+            <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
